f54d2a2e90939dd0849b27cc369d96879f3183bc
Askar <oskarelectrotrans@gmail.com>
2022-07-19 23:17:28 +0300

    Task 1 improved

--- Task_1.py
+++ Task_1.py
@@ -1,4 +1,5 @@
 #1 - Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+import random
 
 def odd_position(numbers: list):
     sum = 0
@@ -8,6 +9,15 @@ def odd_position(numbers: list):
     return sum
 
 
-num_list = [2, 3, 5, 9, 3, 8]
+def create_rnd_list():
+    rnd_list = []
+    for i in range(10):
+        rnd_list.append(random.randint(0, 6))
+    print(rnd_list)
+    return rnd_list
+
+
+
+num_list = create_rnd_list()
 print(odd_position(num_list))
 
